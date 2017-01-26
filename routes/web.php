b1f165f1d3dc05f458c8d5946fcebd8451e3d88c
Felipe Panegalli <felipe.panegalli@gmail.com>
2017-01-26 09:22:58 -0200

    - Criado Login com controle de usuÃrios

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/user', function(){
+    Auth::loginUsingId(2);
+    return "Logado com Teste Cliente";
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return redirect()->route('admin.home');
+});
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function (){
+
+    Auth::routes();
+
+    Route::group(['middleware' => 'can:access-admin'], function (){
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+});
+
